5c8dd810e5c621f26305730c015eb487712807b1
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-06-27 22:41:12 -0600

    Refactor MATCHES16 array to single line format

--- matches16.js
+++ matches16.js
@@ -1,11 +1,5 @@
 export const MATCHES16 = [
   {
-    id: "R32-001",
-    date: "2026-06-28",
-    time: "13:00",
-    group: "16avos",
-    home: "Equipo A",
-    away: "Equipo B",
-    venue: "Sede"
+    id: "R32-001", date: "2026-06-28", time: "13:00", group: "16avos", home: "Equipo A", away: "Equipo B", venue: "Sede"
   }
 ];
