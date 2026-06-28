8c9f5b5769b8fbd59bf4a61d14eba2765461b584
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-06-27 22:57:03 -0600

    Update matches16.js

--- matches16.js
+++ matches16.js
@@ -1,5 +1,146 @@
 export const MATCHES16 = [
   {
-    id: "R32-001", date: "2026-06-28", time: "13:00", group: "16avos", home: "Equipo A", away: "Equipo B", venue: "Sede"
+    id: "R32-001",
+    date: "2026-06-28",
+    time: "13:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-002",
+    date: "2026-06-28",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-003",
+    date: "2026-06-28",
+    time: "19:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-004",
+    date: "2026-06-29",
+    time: "13:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-005",
+    date: "2026-06-29",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-006",
+    date: "2026-06-29",
+    time: "19:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-007",
+    date: "2026-06-30",
+    time: "13:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-008",
+    date: "2026-06-30",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-009",
+    date: "2026-06-30",
+    time: "19:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-010",
+    date: "2026-07-01",
+    time: "13:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-011",
+    date: "2026-07-01",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-012",
+    date: "2026-07-01",
+    time: "19:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-013",
+    date: "2026-07-02",
+    time: "13:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-014",
+    date: "2026-07-02",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-015",
+    date: "2026-07-02",
+    time: "19:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
+  },
+  {
+    id: "R32-016",
+    date: "2026-07-03",
+    time: "16:00",
+    group: "16avos",
+    home: "Equipo A",
+    away: "Equipo B",
+    venue: "Sede pendiente"
   }
 ];
