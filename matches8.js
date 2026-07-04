8a076b50e8cb9e6e70e2ac2e6edfb48eb8fdc1d7
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-07-04 12:16:23 -0600

    Update matches8.js

--- matches8.js
+++ matches8.js
@@ -1,1 +1,83 @@
+export const MATCHES8 = [
 
+  {
+    id: "R16-001",
+    date: "2026-07-04",
+    time: "13:00",
+    group: "Octavos",
+    home: "Canadá",
+    away: "Marruecos",
+    venue: "NRG Stadium, Houston"
+  },
+
+  {
+    id: "R16-002",
+    date: "2026-07-04",
+    time: "17:00",
+    group: "Octavos",
+    home: "Paraguay",
+    away: "Francia",
+    venue: "Lincoln Financial Field, Philadelphia"
+  },
+
+  {
+    id: "R16-003",
+    date: "2026-07-05",
+    time: "16:00",
+    group: "Octavos",
+    home: "Brasil",
+    away: "Noruega",
+    venue: "MetLife Stadium, East Rutherford (New York / New Jersey)"
+  },
+
+  {
+    id: "R16-004",
+    date: "2026-07-05",
+    time: "20:00",
+    group: "Octavos",
+    home: "México",
+    away: "Inglaterra",
+    venue: "Estadio Azteca, Ciudad de México"
+  },
+
+  {
+    id: "R16-005",
+    date: "2026-07-06",
+    time: "15:00",
+    group: "Octavos",
+    home: "Portugal",
+    away: "España",
+    venue: "AT&T Stadium, Arlington"
+  },
+
+  {
+    id: "R16-006",
+    date: "2026-07-06",
+    time: "20:00",
+    group: "Octavos",
+    home: "Estados Unidos",
+    away: "Bélgica",
+    venue: "Lumen Field, Seattle"
+  },
+
+  {
+    id: "R16-007",
+    date: "2026-07-07",
+    time: "12:00",
+    group: "Octavos",
+    home: "Argentina",
+    away: "Egipto",
+    venue: "Mercedes-Benz Stadium, Atlanta"
+  },
+
+  {
+    id: "R16-008",
+    date: "2026-07-07",
+    time: "16:00",
+    group: "Octavos",
+    home: "Suiza",
+    away: "Colombia",
+    venue: "BC Place, Vancouver"
+  }
+
+];
