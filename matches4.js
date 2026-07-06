aa8ee5c27b80f33b27cc9f6b907684177262bc12
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-07-06 10:56:43 -0600

    Create matches4.js

---
+++ matches4.js
@@ -0,0 +1,43 @@
+export const MATCHES4 = [
+
+  {
+    id: "R4-001",
+    date: "2026-07-10",
+    time: "13:00",
+    group: "Cuartos",
+    home: "Ganador Octavos 1",
+    away: "Ganador Octavos 2",
+    venue: "Gillette Stadium, Foxborough"
+  },
+
+  {
+    id: "R4-002",
+    date: "2026-07-10",
+    time: "17:00",
+    group: "Cuartos",
+    home: "Ganador Octavos 3",
+    away: "Ganador Octavos 4",
+    venue: "SoFi Stadium, Los Ángeles"
+  },
+
+  {
+    id: "R4-003",
+    date: "2026-07-11",
+    time: "13:00",
+    group: "Cuartos",
+    home: "Ganador Octavos 5",
+    away: "Ganador Octavos 6",
+    venue: "Hard Rock Stadium, Miami"
+  },
+
+  {
+    id: "R4-004",
+    date: "2026-07-11",
+    time: "17:00",
+    group: "Cuartos",
+    home: "Ganador Octavos 7",
+    away: "Ganador Octavos 8",
+    venue: "Arrowhead Stadium, Kansas City"
+  }
+
+];
