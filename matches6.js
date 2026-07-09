9a1432cbec8b9cf21bd1520d8dd9b813e8d25893
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-07-09 09:58:04 -0600

    Create matches6.js

---
+++ matches6.js
@@ -0,0 +1,23 @@
+export const MATCHES6 = [
+
+  {
+    id: "R6-001",
+    date: "2026-07-14",
+    time: "14:00",
+    group: "Semifinal",
+    home: "Ganador Cuartos 1",
+    away: "Ganador Cuartos 2",
+    venue: "AT&T Stadium, Arlington"
+  },
+
+  {
+    id: "R6-002",
+    date: "2026-07-15",
+    time: "14:00",
+    group: "Semifinal",
+    home: "Ganador Cuartos 3",
+    away: "Ganador Cuartos 4",
+    venue: "Mercedes-Benz Stadium, Atlanta"
+  }
+
+];
