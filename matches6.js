a5b7890dd2d2f99b130d04bb1232f4023671961c
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-07-13 08:26:48 -0600

    Update match times and teams for semifinals

--- matches6.js
+++ matches6.js
@@ -3,20 +3,20 @@ export const MATCHES6 = [
   {
     id: "R6-001",
     date: "2026-07-14",
-    time: "14:00",
+    time: "13:00",
     group: "Semifinal",
-    home: "Ganador Cuartos 1",
-    away: "Ganador Cuartos 2",
+    home: "Francia",
+    away: "España",
     venue: "AT&T Stadium, Arlington"
   },
 
   {
     id: "R6-002",
     date: "2026-07-15",
-    time: "14:00",
+    time: "13:00",
     group: "Semifinal",
-    home: "Ganador Cuartos 3",
-    away: "Ganador Cuartos 4",
+    home: "Inglaterra",
+    away: "Argentina",
     venue: "Mercedes-Benz Stadium, Atlanta"
   }
 
