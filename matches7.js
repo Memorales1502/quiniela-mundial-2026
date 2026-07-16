62db7be50a08178e65ec8afbe9d34d7c75910baa
Memorales1502 <163552575+Memorales1502@users.noreply.github.com>
2026-07-16 11:41:04 -0600

    Create matches7.js

---
+++ matches7.js
@@ -0,0 +1,23 @@
+export const MATCHES7 = [
+
+  {
+    id: "R7-001",
+    date: "2026-07-18",
+    time: "15:00",
+    group: "Tercer Lugar",
+    home: "Francia",
+    away: "Inglaterra",
+    venue: "Hard Rock Stadium, Miami"
+  },
+
+  {
+    id: "R7-002",
+    date: "2026-07-19",
+    time: "13:00",
+    group: "Final",
+    home: "España",
+    away: "Argentina",
+    venue: "MetLife Stadium, East Rutherford"
+  }
+
+];
